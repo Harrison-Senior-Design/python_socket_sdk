4eab179e8785de3108b30a1cd8bef4fd1afcce7d
John Silva <minemidnightmc@gmail.com>
2022-11-08 05:49:16 +0000

    working socket code

--- core/socket.py
+++ core/socket.py
@@ -1,10 +1,14 @@
 import zmq
+from core.protobuf_files.generated import main_pb2, simulationMessages_pb2
+from pyee import EventEmitter
+
 
 class Socket:
-    SOCKET_TYPE = zmq.ROUTER;
+    SOCKET_TYPE = zmq.ROUTER
 
     def __init__(self, addr):
         self.addr = addr
+        self.emitter = EventEmitter()
 
         ctx = zmq.Context()
         self.socket = ctx.socket(Socket.SOCKET_TYPE)
@@ -12,5 +16,26 @@ class Socket:
     def connect(self):
         self.socket.bind(self.addr)
 
-    def recv(self):
-        return self.socket.recv()
+    async def recv_handler(self) -> main_pb2.Wrapper:
+        data = self.socket.recv()
+
+        wrapped_message = main_pb2.Wrapper()
+
+        wrapped_message.ParseFromString(data)
+
+        self.emitter.emit("message", wrapped_message)
+
+        opcode = wrapped_message.opcode
+
+        if opcode == main_pb2.OperationCode.ANGLE_DATA:
+            payload = simulationMessages_pb2.AnglePayload()
+            wrapped_message.payload.Unpack(payload)
+
+            self.emitter.emit("angle", payload)
+        elif opcode == main_pb2.OperationCode.MOVE_STARTING_LOCATION:
+            payload = simulationMessages_pb2.AnglePayload()
+            wrapped_message.payload.Unpack(payload)
+
+            self.emitter.emit("move_starting_location", payload)
+        else:
+            self.emitter.emit("unknown_payload", wrapped_message)
--- test.py
+++ test.py
@@ -1,15 +1,21 @@
 from core.socket import Socket
+import asyncio
 
-def main():
-	socket = Socket("tcp://127.0.0.1:5556")
+socket = Socket("tcp://127.0.0.1:5556")
 
-	print(f"Connecting to {socket.addr} with type {socket.SOCKET_TYPE}")
-	socket.connect()
 
-	while True:
-		print("Waiting for message")
-		message = socket.socket.recv()
-		print("message received: " + str(message))
+@socket.emitter.on("angle")
+def handle_angle_payload(payload):
+    print(f"angle handler: {payload.angle}")
 
-if __name__ == "__main__":
-	main()
+
+async def main():
+    print(f"Connecting to {socket.addr} with type {socket.SOCKET_TYPE}")
+    socket.connect()
+
+    while True:
+        await socket.recv_handler()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
