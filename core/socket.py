73abf02a9a8f43c593d09d764703066ba4f17f10
John Silva <minemidnightmc@gmail.com>
2023-02-05 14:48:07 -0500

    update protobuf_files

--- core/socket.py
+++ core/socket.py
@@ -1,10 +1,11 @@
 import zmq
 import threading
-import time
 
-import core.protobuf_files.generated.main_pb2 as main_proto
-import core.protobuf_files.generated.hardwareMessages_pb2 as hardware_proto
-import core.protobuf_files.generated.simulationMessages_pb2 as simulation_proto
+from google.protobuf.any_pb2 import Any
+
+from ..core.protobuf_files.generated import main_pb2 as main_proto
+from ..core.protobuf_files.generated import hardwareMessages_pb2 as hardware_proto
+from ..core.protobuf_files.generated import simulationMessages_pb2 as simulation_proto
 
 class Emitter:
     def __init__(self):
@@ -33,14 +34,27 @@ class Emitter:
 class Socket:
     SOCKET_TYPE = zmq.DEALER
 
-    def __init__(self, address):
+    def __init__(self, address, identity):
         self.context = zmq.Context()
-        self.socket = self.context.socket(Socket.SOCKET_TYPE)
         self.address = address;
-        self.socket.connect(address)
         self.emitter = Emitter()
         self.running = False
         self.thread = None
+        self.identity = identity;
+
+        self.socket = self.context.socket(Socket.SOCKET_TYPE)
+        self.socket.setsockopt(zmq.LINGER, 0)
+        self.socket.setsockopt(zmq.HEARTBEAT_IVL, 1000)  # 1000ms interval
+        self.socket.setsockopt(zmq.HEARTBEAT_TIMEOUT, 3000)  # 3000ms timeout
+        self.socket.setsockopt(zmq.HEARTBEAT_TTL, 1750)  # 1750ms ttl
+        self.socket.setsockopt(zmq.RECONNECT_IVL, 5000)  # 5000ms reconnect interval
+        self.socket.setsockopt(zmq.RECONNECT_IVL_MAX, 60000)  # 60000ms maximum reconnect interval
+        self.socket.setsockopt_string(zmq.IDENTITY, identity)
+
+        self.socket.connect(address)
+
+        self.poller = zmq.Poller()
+        self.poller.register(self.socket, zmq.POLLIN)
 
     def start(self):
         self.running = True
@@ -49,32 +63,52 @@ class Socket:
 
     def stop(self):
         self.running = False
+        print("Shutting down recv thread (takes up to 4000ms)")
         self.thread.join()
+
+    def cleanup(self):
+        self.stop()
+        self.poller.unregister(self.socket)
+
+        print("Closing socket")
+        self.socket.close()
+        self.context.term()
 
     def _receive(self):
         while self.running:
-            msg = self.socket.recv_multipart()
+            socks = dict(self.poller.poll(4000))
 
-            if len(msg) < 2:
-                continue
-            identity, message = msg[0], msg[1:]
+            if not self.running:
+                return
 
-            wrapper = main_proto.Wrapper()
-            wrapper.ParseFromString(message[0])
-            
-            if wrapper.opcode == main_proto.OperationCode.ANGLE_DATA:
-                payload = simulation_proto.AnglePayload()
-                wrapper.payload.Unpack(payload)
-                self.emitter.emit("angle", payload)
-            elif wrapper.opcode == main_proto.OperationCode.MOVE_STARTING_LOCATION:
-                payload = hardware_proto.MoveStarterLocationPayload()
-                wrapper.payload.Unpack(payload)
-                self.emitter.emit("move_starter_location", payload)
-            else:
-                self.emitter.emit("unknown_payload", wrapped_message)
+            if self.socket in socks and socks[self.socket] == zmq.POLLIN:
+                self._recv_handle()
+
+    def _recv_handle(self):
+        multipart_msg = self.socket.recv_multipart()
+
+        if len(multipart_msg) < 2:
+            return
+
+        print("Full message: ", multipart_msg)
+        message = multipart_msg[1]
+
+        wrapper = main_proto.Wrapper()
+        wrapper.ParseFromString(message[0])
+        
+        if wrapper.opcode == main_proto.OperationCode.ANGLE_DATA:
+            payload = simulation_proto.AnglePayload()
+            wrapper.payload.Unpack(payload)
+            self.emitter.emit("angle", payload)
+        elif wrapper.opcode == main_proto.OperationCode.MOVE_STARTING_LOCATION:
+            payload = hardware_proto.MoveStarterLocationPayload()
+            wrapper.payload.Unpack(payload)
+            self.emitter.emit("move_starter_location", payload)
+        else:
+            self.emitter.emit("unknown_payload", wrapper)
     
     def send_message(self, opcode, payload):
-        wrapper = Main.Wrapper(
+        wrapper = main_proto.Wrapper(
             opcode=opcode,
             timestamp=0,
             payload=Any(value=payload.SerializeToString())
